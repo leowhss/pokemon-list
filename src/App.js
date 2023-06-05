f5fbca58dca0ae46cb1e59b482411fe1e658753e
leowhss <leowhss@yahoo.com>
2023-06-05 22:47:34 +0800

    Added Proptypes.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
-import logo from './logo.svg';
+
+import React from "react";
+import PropTypes from "prop-types";
 import './App.css';
 import pokemon from "./pokemon.json"
+
+const PokemonRow = ({ pokemon }) => (
+  <>
+  <tr>
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(",")}</td>
+  </tr>
+  </>
+);
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.shape({
+        english: PropTypes.string.isRequired,
+      }),
+      type: PropTypes.arrayOf(PropTypes.string.isRequired),
+    })
+  ),
+};
+
 
 function App() {
   return <div
@@ -22,10 +46,8 @@ function App() {
       </thead>
       <tbody>
         {pokemon.slice(0, 20).map(pokemon => (
-          <tr key={pokemon.id}>
-            <td>{pokemon.name.english}</td>
-            <td>{pokemon.type.join(",")}</td>
-          </tr>))}
+          <PokemonRow pokemon={pokemon} />
+        ))}
       </tbody>
     </table>
 
